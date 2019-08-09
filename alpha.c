a3ecddd357a71b8b8aec8ba6291a5d06c481366d
sandhyapalanivel <52828465+sandhyapalanivel@users.noreply.github.com>
2019-08-09 14:38:59 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	char ch;
+	scanf("%c",&ch);
+if(ch>='a'&&ch<='z')
+     {
+     	printf("alphabet");
+    }
+     	else 
+{
+     	printf("not");
+}
+ 
+	return 0;
+}
