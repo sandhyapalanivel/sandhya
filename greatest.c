2899042b1a052185f09f779cd5e36ab4e6aa4291
sandhyapalanivel <52828465+sandhyapalanivel@users.noreply.github.com>
2019-08-09 14:49:50 +0530

    Create greatest.c

---
+++ greatest.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	int a=6,b=7,c=8;
+if(a>b&&a>c)
+     {
+     	printf("a is greater");
+    }
+     	else if (b>a&&b>c) 
+{
+     	printf("b is greater");
+}
+else if(c>a&&c&&b)
+{
+printf("c is greater");	
+}
+ 
+	return 0;
+}
